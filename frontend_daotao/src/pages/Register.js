40aefc46c7cdf848aa6e6f49e6f9dd97ef1dd8d6
phanvan2 <phanvanphung215@gmail.com>
2023-08-16 16:24:59 +0700

    update UI

---
+++ frontend_daotao/src/pages/Register.js
@@ -0,0 +1,38 @@
+import "../css/Login.css" ; 
+
+function Register() {
+    
+    return(
+        <>
+            <div class="form-wrapper">
+                <h2>Sign Up</h2>
+                <form action="#">
+                    <div class="form-control">
+                        <input type="text" defaultValue={""} required/>
+                        <label>Email or phone number</label>
+                    </div>
+                    <div class="form-control">
+                        <input type="text" defaultValue={""} required/>
+                        <label>Name</label>
+                    </div>
+                    <div class="form-control">
+                        <input type="password" defaultValue={""} required/>
+                        <label>Password</label>
+                    </div>
+                    <div class="form-control">
+                        <input type="password" defaultValue={""} required/>
+                        <label>Re Password</label>
+                    </div>
+                    <button type="submit">Sign In</button>
+                    <div class="form-help"> 
+                       
+                        <a href="#">Need help?</a>
+                    </div>
+                </form>
+                
+            </div>
+        </>
+    )
+}
+
+export default Register ; 
